b6bab9909db5e70a10fe5ad6ac723acfa76ae9e6
Hoa Nguyen <ngocuong972000@gmail.com>
2024-06-07 00:47:38 +0700

    feat: update pdf lesson ui

--- pages/course/[slug]/learn/[lessonSlug].tsx
+++ pages/course/[slug]/learn/[lessonSlug].tsx
@@ -3,8 +3,13 @@ import LearnLayout from '@/components/layout/learn-layout';
 import VideoPlayer from '@/components/video-player';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import styles
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export default function LearnCourse() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const pdfUrl =
     'https://res.cloudinary.com/dvz7322mp/raw/upload/v1717171846/hlm-dev/lesson-document/6/yojh32bdtja8nxl2qxwx.pdf';
   return (
@@ -20,12 +25,15 @@ export default function LearnCourse() {
           ],
         }}
       /> */}
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <Viewer
-          fileUrl={pdfUrl}
-          // plugins={[defaultLayoutPluginInstance]}
-        />
-      </Worker>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <Viewer
+            fileUrl={pdfUrl}
+            plugins={[defaultLayoutPluginInstance]}
+            defaultScale={1.5}
+            theme="dark"
+          />
+        </Worker>
+
       <Text size="xl" className="!font-medium !text-gray-700 px-10 py-5">
         JavaScript Development Workbook
       </Text>
