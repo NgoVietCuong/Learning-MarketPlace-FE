a77a73c760e33b00420c03bff56e95fc054bd767
Hoa Nguyen <ngocuong972000@gmail.com>
2024-06-14 18:09:53 +0700

    feat: complete reviews ui

--- pages/course/[slug]/index.tsx
+++ pages/course/[slug]/index.tsx
@@ -5,18 +5,20 @@ import { GetServerSidePropsContext } from 'next';
 import { IoInformationCircle } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import { TbPlayerPlayFilled } from 'react-icons/tb';
-import { Globe, Layers, Dot, Loader2, Newspaper } from 'lucide-react';
-import { IoIosInfinite } from "react-icons/io";
-import { PiMonitorPlay } from "react-icons/pi";
+import { Globe, Layers, Loader2, Newspaper, User } from 'lucide-react';
+import { IoIosInfinite } from 'react-icons/io';
+import { PiMonitorPlay } from 'react-icons/pi';
+import { FaStar } from 'react-icons/fa';
 import { Rate } from 'antd';
 import { Img } from '@/components/ui/img';
 import { Text } from '@/components/ui/text';
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
-import { Progress } from "@/components/ui/progress"
+import { Progress } from '@/components/ui/progress';
 import { useToast } from '@/components/ui/use-toast';
 import DynamicIcon from '@/components/dynamic-icon';
 import CoursePreview from '@/components/modal/CoursePreview';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import useUser from '@/hooks/useUser';
 import useReviews from '@/hooks/useReviews';
@@ -33,8 +35,9 @@ export default function CourseSlugDetails({ slug }: CourseSlugDetailsProps) {
   const router = useRouter();
   const { toast } = useToast();
   const { user } = useUser();
+  const [rating, setRating] = useState<number | undefined>();
   const { courseSlugInfo, courseSlugLoading } = useCourseSlug(slug);
-  const { reviewList, reviewLoading } = useReviews(slug);
+  const { reviewList } = useReviews(slug, rating);
   const [previewOpen, setPreviewOpen] = useState(false);
   const [enrolling, setEnrolling] = useState(false);
 
@@ -52,6 +55,10 @@ export default function CourseSlugDetails({ slug }: CourseSlugDetailsProps) {
         description: `Enrolled course successfully!`,
       });
     }
+  };
+
+  const handleGetReviews = (value: number) => {
+    setRating(value);
   };
 
   return (
@@ -134,15 +141,23 @@ export default function CourseSlugDetails({ slug }: CourseSlugDetailsProps) {
                         />
                       </>
                     )}
-                    <Img className='w-full h-full' src={courseSlugInfo?.data?.imagePreview!} alt="course image preview" />
+                    <Img
+                      className="w-full h-full"
+                      src={courseSlugInfo?.data?.imagePreview!}
+                      alt="course image preview"
+                    />
                   </div>
                   <div className="px-8 py-6 space-y-5 bg-white-primary">
                     {courseSlugInfo?.data?.hasEnrolled ? (
-                      <Button size="lg" className="w-full bg-teal-secondary text-white-primary" onClick={() => router.push(`/course/${slug}/learn/${courseSlugInfo?.data?.currentLesson.id}`)}>
+                      <Button
+                        size="lg"
+                        className="w-full bg-teal-secondary text-white-primary"
+                        onClick={() => router.push(`/course/${slug}/learn/${courseSlugInfo?.data?.currentLesson.id}`)}
+                      >
                         Go to course
                       </Button>
                     ) : courseSlugInfo?.data?.price ? (
-                      <div className='space-y-2'>
+                      <div className="space-y-2">
                         <Heading size="6xl" as="h2" className="font-semibold text-gray-700">
                           ${courseSlugInfo?.data?.price}
                         </Heading>
@@ -160,21 +175,38 @@ export default function CourseSlugDetails({ slug }: CourseSlugDetailsProps) {
                         {enrolling && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}Enroll course
                       </Button>
                     )}
-                    <div className='flex flex-col gap-3'>
-                      <Text size="sm" className='!font-medium !text-gray-700'>This course includes:</Text>
-                      {courseSlugInfo?.data?.totalVideoDuration && (<Text size="tx" className='!text-gray-700 inline-flex items-center'><PiMonitorPlay className='w-[19px] h-[19px] mr-2 text-gray-700' />{courseSlugInfo?.data?.totalVideoDuration} high quality video</Text>)}
-                      {courseSlugInfo?.data?.totalArticles && (<Text size="tx" className='!text-gray-700 inline-flex items-center'><Newspaper className='w-[17px] h-[17px] mr-2 text-gray-600' />{courseSlugInfo?.data?.totalArticles} articles</Text>)}
-                      <Text size="tx" className='!text-gray-700 inline-flex items-center'><IoIosInfinite  className='w-[18px] h-[18px] mr-2 text-gray-700' />Full lifetime access</Text>
+                    <div className="flex flex-col gap-3">
+                      <Text size="sm" className="!font-medium !text-gray-700">
+                        This course includes:
+                      </Text>
+                      {courseSlugInfo?.data?.totalVideoDuration && (
+                        <Text size="tx" className="!text-gray-700 inline-flex items-center">
+                          <PiMonitorPlay className="w-[19px] h-[19px] mr-2 text-gray-700" />
+                          {courseSlugInfo?.data?.totalVideoDuration} high quality video
+                        </Text>
+                      )}
+                      {courseSlugInfo?.data?.totalArticles && (
+                        <Text size="tx" className="!text-gray-700 inline-flex items-center">
+                          <Newspaper className="w-[17px] h-[17px] mr-2 text-gray-600" />
+                          {courseSlugInfo?.data?.totalArticles} articles
+                        </Text>
+                      )}
+                      <Text size="tx" className="!text-gray-700 inline-flex items-center">
+                        <IoIosInfinite className="w-[18px] h-[18px] mr-2 text-gray-700" />
+                        Full lifetime access
+                      </Text>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-full p-0">
+          <div className="w-full p-0 ">
             <div className="container px-20 py-10 space-y-10">
               <div className="space-y-3 w-3/5">
-                <Heading size='4xl' className="!font-semibold !text-gray-800">Course content</Heading>
+                <Heading size="4xl" className="!font-semibold !text-gray-800">
+                  Course content
+                </Heading>
                 <Accordion type="multiple">
                   {courseSlugInfo?.data?.sections?.map((section) => (
                     <AccordionItem
@@ -224,40 +256,181 @@ export default function CourseSlugDetails({ slug }: CourseSlugDetailsProps) {
               </div>
 
               <div className="space-y-3 w-3/5">
-                <Heading size='4xl' className="!font-semibold !text-gray-800">Description</Heading>
+                <Heading size="4xl" className="!font-semibold !text-gray-800">
+                  Description
+                </Heading>
                 <div
                   className="description text-[13px]"
                   dangerouslySetInnerHTML={{ __html: courseSlugInfo?.data?.description! }}
                 ></div>
               </div>
-
+            </div>
+          </div>
+          <div className="w-full p-0 border-t">
+            <div className="container px-20 py-10 space-y-10 ">
               <div className="space-y-3 w-full">
-                <div className="flex items-center">
-                  <Rate className=" text-yellow-500 mr-2" count={1} defaultValue={1} />
-                  <Heading size='4xl' className="!font-semibold !text-gray-800 mr-1">
-                    {courseSlugInfo?.data?.averageRating} course rating
-                  </Heading>
-                  <Dot className="w-10 h-10 text-gray-700" />
-                  <Heading  size='4xl' className="!font-semibold !text-gray-800">
-                    {courseSlugInfo?.data?.totalReviews! > 1
-                      ? `${courseSlugInfo?.data?.totalReviews} ratings`
-                      : `${courseSlugInfo?.data?.totalReviews} rating`}
-                  </Heading>
-                </div>
-
-                <div>{reviewLoading ? <></> : <Button variant={'outline'}>Show all reviews</Button>}</div>
-                <div className='flex'>
-                  <div className="flex flex-col w-1/4">
-                    <Text>Overall rating</Text>
-                    <Rate />
-
-                    <div className="flex items-center gap-3">
-                      <Text>5</Text>
-                      <Progress />
-                      <Text>3</Text>
+                <Heading size="4xl" className="!font-semibold !text-gray-800">
+                  Reviews <span className="text-base">({courseSlugInfo?.data?.totalReviews})</span>
+                </Heading>
+                <div className="flex gap-28">
+                  <div className="flex flex-col w-[26%] gap-4">
+                    <div>
+                      <Text size="s" className="!font-semibold !text-gray-700">
+                        Overall rating
+                      </Text>
+                      <Text size="2xl" className="!font-semibold !text-gray-700 inline-flex items-center gap-2">
+                        {courseSlugInfo?.data?.averageRating}
+                        <Rate
+                          disabled
+                          allowHalf
+                          defaultValue={Number(courseSlugInfo?.data?.averageRating)}
+                          className="text-base text-yellow-500"
+                        />
+                      </Text>
+                    </div>
+                    <div className="space-y-1">
+                      <div className={`flex items-center gap-3 cursor-pointer ${(rating && rating !== 5) ? 'opacity-30' : '' }`} onClick={() => handleGetReviews(5)}>
+                        <div className="flex justify-between items-center !w-[27px] min-w-[27px]">
+                          <Text size="sm" className="!font-medium !text-gray-700 inline-flex items-center">
+                            5
+                          </Text>
+                          <FaStar className="text-xs text-yellow-500" />
+                        </div>
+                        <Progress
+                          value={
+                            (Number(courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 5)?.count) /
+                              courseSlugInfo?.data?.totalReviews!) *
+                            100
+                          }
+                        />
+                        <Text
+                          size="sm"
+                          className="!font-medium !text-gray-700 underline"
+                        >
+                          {courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 5)?.count}
+                        </Text>
+                      </div>
+                      <div className={`flex items-center gap-3 cursor-pointer ${(rating && rating !== 4) ? 'opacity-30' : '' }`} onClick={() => handleGetReviews(4)}>
+                        <div className="flex justify-between items-center !w-[27px] min-w-[27px]">
+                          <Text size="sm" className="!font-medium !text-gray-700 inline-flex items-center">
+                            4
+                          </Text>
+                          <FaStar className="text-xs text-yellow-500" />
+                        </div>
+                        <Progress
+                          value={
+                            (Number(courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 4)?.count) /
+                              courseSlugInfo?.data?.totalReviews!) *
+                            100
+                          }
+                        />
+                        <Text
+                          size="sm"
+                          className="!font-medium !text-gray-700 underline"
+                        >
+                          {courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 4)?.count}
+                        </Text>
+                      </div>
+                      <div className={`flex items-center gap-3 cursor-pointer ${(rating && rating !== 3) ? 'opacity-30' : '' }`} onClick={() => handleGetReviews(3)}>
+                        <div className="flex justify-between items-center !w-[27px] min-w-[27px]">
+                          <Text size="sm" className="!font-medium !text-gray-700 inline-flex items-center">
+                            3
+                          </Text>
+                          <FaStar className="text-xs text-yellow-500" />
+                        </div>
+                        <Progress
+                          value={
+                            (Number(courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 3)?.count) /
+                              courseSlugInfo?.data?.totalReviews!) *
+                            100
+                          }
+                        />
+                        <Text
+                          size="sm"
+                          className="!font-medium !text-gray-700 underline"
+                        >
+                          {courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 3)?.count}
+                        </Text>
+                      </div>
+                      <div className={`flex items-center gap-3 cursor-pointer ${(rating && rating !== 2) ? 'opacity-30' : '' }`} onClick={() => handleGetReviews(2)}>
+                        <div className="flex justify-between items-center !w-[27px] min-w-[27px]">
+                          <Text size="sm" className="!font-medium !text-gray-700 inline-flex items-center">
+                            2
+                          </Text>
+                          <FaStar className="text-xs text-yellow-500" />
+                        </div>
+                        <Progress
+                          value={
+                            (Number(courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 2)?.count) /
+                              courseSlugInfo?.data?.totalReviews!) *
+                            100
+                          }
+                        />
+                        <Text
+                          size="sm"
+                          className="!font-medium !text-gray-700 underline"
+                        >
+                          {courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 2)?.count}
+                        </Text>
+                      </div>
+                      <div className={`flex items-center gap-3 cursor-pointer ${(rating && rating !== 1) ? 'opacity-30' : '' }`} onClick={() => handleGetReviews(1)}>
+                        <div className="flex justify-between items-center !w-[27px] min-w-[27px]">
+                          <Text size="sm" className="!font-medium !text-gray-700 inline-flex items-center">
+                            1
+                          </Text>
+                          <FaStar className="text-xs text-yellow-500" />
+                        </div>
+                        <Progress
+                          value={
+                            (Number(courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 1)?.count) /
+                              courseSlugInfo?.data?.totalReviews!) *
+                            100
+                          }
+                        />
+                        <Text
+                          size="sm"
+                          className="!font-medium !text-gray-700 underline"
+                        >
+                          {courseSlugInfo?.data?.numberEachRatings.find((item) => item.rate === 1)?.count}
+                        </Text>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex flex-col w-3/4"></div>
+                  <div className="flex flex-col w-[60%]">
+                    {reviewList && (
+                      <div className="flex flex-col gap-6 h-full">
+                        {reviewList?.data?.items?.length! === 0 && <div className='w-full h-full flex justify-center items-center'><Text>No reviews found</Text></div>}
+                        {reviewList?.data?.items?.length! > 0 && reviewList?.data?.items.map((item) => (
+                          <div className="flex flex-col gap-4 border-b border-gray-200 pb-6">
+                            <div className="flex justify-between">
+                              <div className="flex items-center gap-3">
+                                <Avatar className="h-9 w-9">
+                                  <AvatarImage
+                                    src={item?.enrollment?.user?.avatar ? item?.enrollment?.user?.avatar : undefined}
+                                  />
+                                  <AvatarFallback className="bg-slate-300 text-white-primary text-center font-medium text-sm">
+                                    <User className="w-6 h-6" />
+                                  </AvatarFallback>
+                                </Avatar>
+                                <div className="space-y-0">
+                                  <Text size="sm" className="!font-medium !text-gray-700">
+                                    {item?.enrollment?.user?.username}
+                                  </Text>
+                                  <Rate className="text-xs text-yellow-500" disabled value={item?.rating} />
+                                </div>
+                              </div>
+                              <Text size="tx" className="">
+                                {item?.updatedAt.substring(0, 10)}
+                              </Text>
+                            </div>
+                            <Text size="sm" className="!text-gray-700">
+                              {item?.comment}
+                            </Text>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
