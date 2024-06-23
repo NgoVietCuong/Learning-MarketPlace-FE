caabc2a89704871fbb2acf810ebd39f54a9714f1
Hoa Nguyen <ngocuong972000@gmail.com>
2024-06-24 00:08:13 +0700

    feat: update settings page

--- pages/instructor/settings.tsx
+++ pages/instructor/settings.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
 import InstructorLayout from "@/components/layout/instructor-layout";
 
 export default function InstructorSettings() {
-  return <div>Instructor Settings</div>
+  return (
+    <div className="grow flex justify-center items-center">
+      <div className="bg-white-primary w-[95%] h-[95%] shadow-lg rounded-xl overflow-y-scroll">
+        <div className="px-10 py-8 flex flex-col gap-8">
+            <Heading className="!font-medium">Instructor Profile</Heading>
+            <div className="w-full flex flex-col gap-5">
+              <Button>Login to paypal</Button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 InstructorSettings.getLayout = function (page: React.ReactNode) {
