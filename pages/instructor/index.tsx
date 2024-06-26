5c54e12f52f7603cb5d9f69e6989851a1b1c76e1
Hoa Nguyen <ngocuong972000@gmail.com>
2024-06-27 00:25:59 +0700

    feat: instructor dashboard

--- pages/instructor/index.tsx
+++ pages/instructor/index.tsx
@@ -1,9 +1,35 @@
-import InstructorLayout from "@/components/layout/instructor-layout";
+import { Text } from '@/components/ui/text';
+import { Heading } from '@/components/ui/heading';
+import InstructorLayout from '@/components/layout/instructor-layout';
 
 export default function InstructorDashboard() {
-  return <>Instructor Dashboard</>
+  return (
+    <div className="grow flex justify-center items-center">
+      <div className="flex flex-col gap-5 w-[95%] h-[95%]">
+        <div className="px-10 flex flex-col gap-8">
+          <Heading className="!font-medium">Dashboard</Heading>
+          <div className="flex items-center gap-5">
+            <div className="flex flex-col items-center justify-center w-[14%] h-[200px] bg-teal-500 rounded-md">
+              <Text>Total Students</Text>
+              <Text>1220</Text>
+            </div>
+            <div className="flex flex-col items-center justify-center w-[14%] h-[200px] bg-teal-500 rounded-md">
+              <Text>Total Reviews</Text>
+              <Text>1220</Text>
+            </div>
+            <div className="flex flex-col items-center justify-center w-[14%] h-[200px] bg-teal-500 rounded-md">
+              <Text>Average Rating</Text>
+            </div>
+            <div className="flex flex-col items-center justify-center w-[14%] h-[200px] bg-teal-500 rounded-md">
+              <Text>Total Income</Text>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 InstructorDashboard.getLayout = function (page: React.ReactNode) {
-  return <InstructorLayout>{page}</InstructorLayout>
-}
+  return <InstructorLayout>{page}</InstructorLayout>;
+};
